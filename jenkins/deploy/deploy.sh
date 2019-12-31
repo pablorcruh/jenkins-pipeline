625c0ae19a5979e55c42c18d9015e176f4300dda
pablorcruh <pablorcruh@gmail.com>
2019-12-31 16:34:49 +0000

    change private key location relative to container

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,8 +4,8 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/pipeline/jenkins/deploy/deploy-key /tmp/.auth prod_user@167.172.125.154:/tmp/.auth
+scp -i /opt/prod /tmp/.auth prod_user@167.172.125.154:/tmp/.auth
 
-scp -i /home/pipeline/jenkins/deploy/deploy-key jenkins/deploy/publish.sh prod_user@167.172.125.154:/tmp/publish
+scp -i /opt/prod jenkins/deploy/publish.sh prod_user@167.172.125.154:/tmp/publish
 
-ssh -i /home/pipeline/jenkins/deploy/deploy-key prod_user@167.172.125.154 "/tmp/publish"
+ssh -i /opt/prod prod_user@167.172.125.154 "/tmp/publish"
