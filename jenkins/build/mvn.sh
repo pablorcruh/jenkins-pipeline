bb640ecddda240bda1946d5c2bec56d66c215357
pablorcruh <pablorcruh@gmail.com>
2021-03-26 23:00:52 -0500

    add env variable to manage project location

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,7 @@ echo "**********************************************************"
 echo "****************    BUILDING JAR       *******************"
 echo "**********************************************************"
 
+WORKSPACE=/home/jenkins_home/workspace/jenkins-pipeline_master
 
 # parametro a pasar ---  mvn -B -DskipTests clean package
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.3-jdk-8 "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.3-jdk-8 "$@"
