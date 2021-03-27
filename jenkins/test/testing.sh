99f55a95384ceb4120ace504d0391066493c3bda
pablorcruh <pablorcruh@gmail.com>
2021-03-26 23:28:30 -0500

    add environment variable on test script

--- jenkins/test/testing.sh
+++ jenkins/test/testing.sh
@@ -4,6 +4,7 @@ echo "**********************************************************"
 echo "****************    TESTING JAR       ********************"
 echo "**********************************************************"
 
+WORKSPACE=/home/jenkins_home/workspace/jenkins-pipeline_master
 
 # parametro a pasar ---  mvn test
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.3-jdk-8 "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.3-jdk-8 "$@"
